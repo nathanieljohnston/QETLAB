234da44273630bfae3066633265c507d410be76e
araujoms <maltusan@gmail.com>
2022-09-20 20:25:59 +0200

    More efficient code in order to calculate the nonsignalling bound

--- helpers/cg2fp.m
+++ helpers/cg2fp.m
@@ -25,8 +25,6 @@ ib = desc(4);
 aindex = @(a,x) (1 + a + (x-1)*(oa-1));
 bindex = @(b,y) (1 + b + (y-1)*(ob-1));
 
-V=zeros(oa,ob,ia,ib);
-
 if behaviour == 0
 
 	for x=1:ia
@@ -66,51 +64,10 @@ elseif behaviour == 1
 
 	for x=1:ia
 		for y=1:ib
-			for a=1:oa-1
-				for b=1:ob-1
-					V(a,b,x,y) = CG(aindex(a,x),bindex(b,y));
-				end
-			end
-		end
-	end
-
-	for x=1:ia
-		for y=1:ib
-			for a=1:oa-1
-				V(a,ob,x,y) = CG(aindex(a,x),1);
-				for b=1:ob-1
-					V(a,ob,x,y) = V(a,ob,x,y) - CG(aindex(a,x),bindex(b,y));
-				end
-			end
-		end
-	end
-
-	
-	for x=1:ia
-		for y=1:ib
-			for b=1:ob-1
-				V(oa,b,x,y) = CG(1,bindex(b,y));
-				for a=1:oa-1
-					V(oa,b,x,y) = V(oa,b,x,y) - CG(aindex(a,x),bindex(b,y));
-				end
-			end
-		end
-	end
-
-	for x=1:ia
-		for y=1:ib
-			V(oa,ob,x,y) = 1;
-			for a=1:oa-1
-				V(oa,ob,x,y) = V(oa,ob,x,y) - CG(aindex(a,x),1);
-			end
-			for b=1:ob-1
-				V(oa,ob,x,y) = V(oa,ob,x,y) - CG(1,bindex(b,y));
-			end
-			for a=1:oa-1
-				for b=1:ob-1
-					V(oa,ob,x,y) = V(oa,ob,x,y) + CG(aindex(a,x),bindex(b,y));
-				end
-			end
+			V(1:oa-1,1:ob-1,x,y) = CG(aindex(1,x):aindex(oa-1,x),bindex(1,y):bindex(ob-1,y));
+			V(1:oa-1,ob,x,y) = CG(aindex(1,x):aindex(oa-1,x),1) - sum(CG(aindex(1,x):aindex(oa-1,x),bindex(1,y):bindex(ob-1,y)),2);
+			V(oa,1:ob-1,x,y) = CG(1,bindex(1,y):bindex(ob-1,y)) - sum(CG(aindex(1,x):aindex(oa-1,x),bindex(1,y):bindex(ob-1,y)),1);
+			V(oa,ob,x,y) = CG(1,1) - sum(CG(aindex(1,x):aindex(oa-1,x),1),1) - sum(CG(1,bindex(1,y):bindex(ob-1,y)),2) + sum(sum(CG(aindex(1,x):aindex(oa-1,x),bindex(1,y):bindex(ob-1,y))));
 		end
 	end
 
