11d2a969f865aeba4b58dc80cca021e0d626ee15
Benjamin-Talbot <=>
2024-08-08 23:19:11 -0300

    add comments to CompoundMatrix.m

--- CompoundMatrix.m
+++ CompoundMatrix.m
@@ -1,21 +1,35 @@
-% if r > min{m, n}, then Cr(A) is the unique 0x0 matrix
-% otherwise, has size (m; r) x (n; r) (m choose r x n choose r)?
+%%  COMPOUNDMATRIX    Computes the rth compound matrix of a given matrix
+%   This function has two required arguments:
+%     A: an arbitrary matrix
+%     r: a positive integer
+%
+%   comp = CompoundMatrix(A, r) returns the rth compound matrix of A
+%
+%   If r > min{m, n}, then the rth compound matrix of A is the 0x0 matrix.
+%   Otherwise, the size of the result is (m choose r) x (n choose r).
+%
+%   URL: http://www.qetlab.com/CompoundMatrix
+%   !! CURRENTLY THERE IS NO PAGE FOR THIS FUNCTION !!
+%             
+%   author: Benjamin Talbot
+%   package: QETLAB
+%   last updated: August 8, 2024
 
-function compMat = compoundMatrix(A, r)
+function comp = CompoundMatrix(A, r)
     m = size(A, 1);
     n = size(A, 2);
     if r > min(m, n)
-        compMat = [];
+        comp = [];
         return
     end
 
     rows = nchoosek(1:m, r);
     cols = nchoosek(1:n, r);
     dims = [size(rows, 1), size(cols, 1)];
-    compMat = zeros(dims);
+    comp = zeros(dims);
     for i = 1:dims(1)
         for j = 1:dims(2)
-            compMat(i, j) = det(A(rows(i, :), cols(j, :)));
+            comp(i, j) = det(A(rows(i, :), cols(j, :)));
         end
     end
 end
