d68bc4ccdf5ec62b6e5a7e1ae31f7d2e798cba01
Benjamin-Talbot <=>
2024-08-25 10:56:16 -0300

    Add hermitian fix for OperatorSchmidtDecomposition, testing environment

--- OperatorSchmidtDecompositionOld.m
+++ OperatorSchmidtDecompositionOld.m
@@ -31,7 +31,7 @@
 %   package: QETLAB
 %   last updated: November 12, 2014
 
-function [s,U,V] = OperatorSchmidtDecomposition(X,varargin)
+function [s,U,V] = OperatorSchmidtDecompositionOld(X,varargin)
 
 dX = size(X);
 sdX = round(sqrt(dX));
@@ -62,90 +62,3 @@ end
 sr = length(s);
 U = mat2cell(reshape(u,dim(1,1),dim(2,1)*sr),dim(1,1),dim(2,1)*ones(1,sr));
 V = mat2cell(reshape(v,dim(1,2),dim(2,2)*sr),dim(1,2),dim(2,2)*ones(1,sr));
-
-% if the input is hermitian, ensure that the output matrices are hermitian
-if isHermitian(X)
-    allhermitian = all(cellfun(@isHermitian, U));
-    if allhermitian
-        allhermitian = all(cellfun(@isHermitian, V));
-    end
-
-    if allhermitian
-        return
-    end
-
-    % Generate hermitian bases for the subsystems
-    basis1 = genHermBasis(dim(1));
-    basis2 = genHermBasis(dim(2));
-    dim1_sq = dim(1)^2;
-    dim2_sq = dim(2)^2;
-
-    % represent X as a coordinate vector in the hermitian basis
-    [j_indices, i_indices] = ndgrid(1:dim1_sq, 1:dim2_sq);
-    kron_basis2 = arrayfun(@(j, i) kron(basis1{j}, basis2{i}), j_indices, i_indices, 'UniformOutput', false);
-    
-    % Reshape the result into a cell array
-    kron_basis2 = reshape(kron_basis2, 1, []);
-    coords = X(:)' * cell2mat(cellfun(@(B) B(:), kron_basis2, 'UniformOutput', false));
-    reshape(coords,[dim1_sq, dim2_sq]);
-
-    % reshape the coordinates into a matrix, compute svd of X in hermitian basis
-    reshaped = reshape(coords,[dim1_sq, dim2_sq]);
-    [A, s, B] = svd(reshaped);
-    s = diag(s);
-
-    % construct U and V from the SVD, ensuring that they are hermitian
-    U = cell(1, length(s));
-    for i = 1:length(s)
-        U{i} = zeros(dim(1));
-        for j = 1:dim1_sq
-            U{i} = U{i} + A(j, i) * basis1{j};
-        end
-    end
-
-    V = cell(1, length(s));
-    for i = 1:length(s)
-        V{i} = zeros(dim(2));
-        for j = 1:dim2_sq
-            V{i} = V{i} + B(j, i) * basis2{j};
-        end
-    end
-end
-
-function isHermitian = isHermitian(X)
-    isHermitian = all(all(abs(X - X') < 1e-10));
-end
-
-function basis = genHermBasis(dim)
-    basis = cell(1, dim^2);
-    ct = 1;
-    for ct = 1:dim
-        elem = sparse(dim, dim);
-        elem(ct, ct) = 1;
-        basis{ct} = elem;
-    end
-    ct = dim + 1;
-
-    for row = 1:dim
-        for col = (row + 1):dim
-            elem = sparse(dim, dim);
-            elem(row, col) = 1;
-            elem(col, row) = 1;
-            elem = elem / sqrt(2);
-            basis{ct} = elem;
-            ct = ct + 1;
-        end
-    end
-
-    for row = 1:dim
-        for col = (row + 1):dim
-            elem = sparse(dim, dim);
-            elem(row, col) = 1i;
-            elem(col, row) = -1i;
-            elem = elem / sqrt(2);
-            basis{ct} = elem;
-            ct = ct + 1;
-        end
-    end
-end
-end
