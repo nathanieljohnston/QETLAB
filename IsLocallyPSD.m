e88e2574126652b1c5b0334462620cb571104b2d
Nathaniel Johnston <nathaniel.johnston@gmail.com>
2024-08-29 14:39:21 -0300

    Updating coherence functions
    - Re-added TraceDistanceCoherence.m (not ready to get rid of it yet, sorry)
    - Renamed IskFactorPositive to IsLocallyPSD, since that's the preferred terminology now
    - Removed ModTraceDist... and Robustnessk... functions for now; they should be refactored at some point to all be one function (e.g., a single ModTraceDist... function that can accept either pure or mixed state input)

--- IsLocallyPSD.m
+++ IsLocallyPSD.m
@@ -1,7 +1,7 @@
-%%  IskFactorPositive    Determines whether or not a matrix is k-factor positive
+%%  IsLocallyPSD    Determines whether or not a matrix is k-locally positive semidefinite
 %   This function has two required arguments:
-%     X: The matrix to check k-factor positivity of
-%     K: The parameter K that determines which level of K-factor positivity
+%     X: The matrix to check k-local PSDness of
+%     K: The parameter K that determines which level of K-locally PSD
 %        is being tested
 %
 %   Returns either 1 or 0, indicating "yes" or "no".
@@ -13,9 +13,9 @@
 %   requires: IsPSD.m
 %   author: Nathaniel Johnston (nathaniel@njohnston.ca)
 %   package: QETLAB
-%   last updated: April 28, 2022
+%   last updated: August 29, 2024
 
-function [ifp,wit] = IskFactorPositive(X,k)
+function [ilp,wit] = IsLocallyPSD(X,k)
 
 sX = length(X);
 wit = 0;
@@ -24,10 +24,10 @@ sub_ind = nchoosek(1:sX(1),k);
 sub_ind_len = size(sub_ind,1);
 for j = 1:sub_ind_len
     if(~IsPSD(X(sub_ind(j,:),sub_ind(j,:))))
-        ifp = 0;
+        ilp = 0;
         wit = [sub_ind(j,:);sub_ind(j,:)];
         return;
     end
 end
 
-ifp = 1;
+ilp = 1;
